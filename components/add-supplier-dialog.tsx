f755a0b71a3cdb3d5cc2979ae01ca846dd44f26e
SKAHLO <egbunaoscar@gmail.com>
2025-08-21 15:10:34 +0100

    updated again

--- components/add-supplier-dialog.tsx
+++ components/add-supplier-dialog.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
